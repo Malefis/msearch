8aa9be86615e8c2096328f3c850ef23cfed4845c
Malefis <infinitecookiehd@gmail.com>
2017-10-07 15:42:41 +0100

    Final ever version of msearch - I hope you enjoy :)

--- js/pricefinder.js
+++
@@ -1,2 +0,0 @@
-$.post("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR,GBP", function(eth) {
-  $('.eth').html(" The current Ethereum exchange rates are : <br>	₿ BTC = "+ eth.BTC + " <br> 	 $ USD= " + eth.USD + " <br>	£ GBP = " + eth.GBP);
